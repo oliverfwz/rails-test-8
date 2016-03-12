035c1787200ac597adc9b71b6ec100488c844269
Oliver Futureworkz <oliver@futureworkz.com>
2016-03-12 14:29:13 +0700

    Update db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+user = User.new(email: 'oliver@futureworkz.com', password: '123123123', name: 'Oliver')
+user.save!
+
+Post.destroy_all
+content = "Maria Sharapova says she is 'determined to fight back' after testing positive for meldonium. <br/>
+          In a Facebook post railing against 'distorted and exaggerated' reporting, she denied taking meldonium every day and missing five warnings that the drug was about to be banned. <br/>
+          She also criticised the tennis authorities for making the relevant information 'too hard to find'."
+Post.create(title: "Maria Sharapova 'determined to fight back' after failed drugs test", content: content)
