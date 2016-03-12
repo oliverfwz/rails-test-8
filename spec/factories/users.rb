d8cf2667da09589b22cfa38c27e538a0dbbe7327
Oliver Futureworkz <oliver@futureworkz.com>
2016-03-12 21:50:56 +0700

    Update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email)      { |n| "user-#{n}@example.com" }
+    password              '123123123'
+    sequence(:name)       "Oliver-#{n}"
   end
 end
