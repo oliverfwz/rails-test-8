63a2b5b504fe956bdfeab6b2e6c500e0dc035648
Oliver Futureworkz <oliver@futureworkz.com>
2016-03-16 00:07:32 +0700

    Add commentable in to comments

---
+++ db/migrate/20160315162341_add_field_in_comments.rb
@@ -0,0 +1,6 @@
+class AddFieldInComments < ActiveRecord::Migration
+  def change
+    add_reference :comments, :commentable, index: true
+    add_column    :comments, :commentable_type, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315145209) do
+ActiveRecord::Schema.define(version: 20160315162341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,11 @@ ActiveRecord::Schema.define(version: 20160315145209) do
     t.text    "message"
     t.integer "user_id"
     t.integer "post_id"
+    t.integer "commentable_id"
+    t.string  "commentable_type"
   end
 
+  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
