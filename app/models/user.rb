fc7f13076efddba26eae003b04d9266c6404e688
Oliver Futureworkz <oliver@futureworkz.com>
2016-03-18 22:54:52 +0700

    Update user + spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ActiveRecord::Base
   has_many :comments, as: :commentable
 
   validates :name,   presence: true
+
+  has_attached_file :avatar,
+    styles: { thumb: '150x150>' },
+    default_url: "users/missing.png"
+  validates_attachment :avatar,
+    content_type: {content_type: /\Aimage\/.*\Z/},
+    size: { in: 0..5.megabytes, message: 'Photo size too large. Please limit to 5 mbs.' }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,16 @@ RSpec.describe User, type: :model do
 
   context 'validations' do
     it { is_expected.to validate_presence_of :name }
+
+    it do
+      is_expected.to validate_attachment_content_type(:avatar)
+        .allowing('image/png', 'image/gif')
+        .rejecting('text/plain', 'text/xml')
+    end
+
+    it do
+      is_expected.to validate_attachment_size(:avatar)
+        .less_than(5.megabytes)
+    end
   end
 end
