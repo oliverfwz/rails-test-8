db60ca9c9019f3662dcb4411deb349cf440b3e53
Oliver Futureworkz <oliver@futureworkz.com>
2016-03-18 21:36:23 +0700

    Update validate commentable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  belongs_to :post
+  belongs_to :commentable, polymorphic: true
 
   validates :message, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   context 'association' do
     it { is_expected.to belong_to :user }
-    it { is_expected.to belong_to :post }
+    it { is_expected.to belong_to :commentable }
   end
   context 'validations' do
     it { is_expected.to validate_presence_of :message }
