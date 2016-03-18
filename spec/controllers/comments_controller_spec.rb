7f6fe57d4557244131bb880c52867dea1d16f9b8
Oliver Futureworkz <oliver@futureworkz.com>
2016-03-19 00:08:06 +0700

    Update rspec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,15 +2,27 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
   describe '#create' do
-    let!(:post) { create :post }
     let!(:user) { create :user }
 
     let(:params) { attributes_for(:comment, user_id: user.id) }
     before  { sign_in user }
 
-    it "save a comment" do
-      coment = post.comments.build(params)
-      expect{ coment.save }.to change(Comment, :count).by(1)
+    context 'Post' do
+      let!(:post) { create :post }
+
+      it "save a post_comment" do
+        coment = post.comments.build(params)
+        expect{ coment.save }.to change(Comment, :count).by(1)
+      end
+    end
+
+    context 'User' do
+      let!(:oliver) { create :user }
+
+      it "save a user_comment" do
+        coment = oliver.comments.build(params)
+        expect{ coment.save }.to change(Comment, :count).by(1)
+      end
     end
   end
 end
